1a64564e8cc9791680cef70a54f39b477af7c35d
Bradley Biera <30395710+bbworks@users.noreply.github.com>
2021-03-04 17:42:17 -0500

    Removal of Unnecessary JS scripts
    OVERVIEW
    Back-End
     o removed docs/js/header.js, docs/js/footer.js in place of EJS partial views
     o removed docs/js/currentPageDetector.js in place of data passed into views/partials/header.ejs

--- docs/js/currentPageDetector.js
+++
@@ -1,9 +0,0 @@
-const CurrentPageDetector = function() {
-  let headerNavContainerNodes = document.getElementsByClassName("header-nav-container")[0].childNodes;
-  for(var i in headerNavContainerNodes) {
-    let element = headerNavContainerNodes[i];
-    if (element.href && element.href === window.location.href) {
-      element.classList.add("current");
-    }
-  }
-}();
